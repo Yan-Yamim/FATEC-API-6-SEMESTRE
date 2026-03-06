f4d187b0d0656db5dc8534c217943527f24c8177
yanyamim <yanyamim@gmail.com>
2026-03-06 16:07:27 -0300

    Including new environments for the settings

--- backend/settings.py
+++ backend/settings.py
@@ -1,9 +1,17 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
-    
+    model_config = SettingsConfigDict(
+        env_file='../.env', 
+        env_file_encoding='utf-8',
+        extra='ignore'
+    )
+
     DATABASE_URL: str
-    DATABASE_NAME: str
-    DATABASE_USERNAME: str  
-    DATABASE_PASSWORD: str
+    POSTGRES_DB: str        
+    POSTGRES_USER: str      
+    POSTGRES_PASSWORD: str  
+    
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
