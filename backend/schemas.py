afef4bcfa8c25c4b2eb42caf7b9ca1a02b87618e
Marília Borgo de Moraes <marilia.bm01@gmail.com>
2026-04-05 09:36:41 -0300

    feat(pk-43): add ETL tasks for loading 'dec_fec' data and corresponding API route (#23)
    * feat(pk-43): add ETL tasks for loading 'dec_fec' data and corresponding API route
    
    * fix: add headers attribute to _FakeStream classes for content-type handling
    
    * fix(pk-43): kill gambiarra

--- backend/schemas.py
+++ backend/schemas.py
@@ -3,3 +3,8 @@ from pydantic import BaseModel, HttpUrl
 
 class DownloadRequest(BaseModel):
     url: HttpUrl
+
+class DecFecRequest(BaseModel):
+    url_realizado: HttpUrl
+    url_limite: HttpUrl
+
